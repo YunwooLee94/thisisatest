1a37a2771bb87770fa7b3c719757e59d9412e59d
leeyunwoo <yunwoo333@gmail.com>
2019-05-16 21:25:14 +0900

    edit_3rd

--- src/talker.cpp
+++ src/talker.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
     {
         std_msgs::String msg;
         std::stringstream ss;
-        ss << "hello wo" <<count;
+        ss << "hello world" <<count;
         msg.data = ss.str();
 
         ROS_INFO("%s", msg.data.c_str());
